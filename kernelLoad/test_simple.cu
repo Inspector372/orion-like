a6778922ac56a1941ed9b890168ef51f878277e9
kechoi <choikang2020@kaist.ac.kr>
2026-05-13 20:21:26 +0900

    original file version error

--- kernelLoad/test_simple.cu
+++ kernelLoad/test_simple.cu
@@ -10,9 +10,9 @@
   TODO: infer function structure from **any** function call.
 
   How to test:
-  !gcc libsmctrl.c -c -o libsmctrl.o -fPIC -I/usr/local/cuda-12.8/targets/x86_64-linux/include
-  !ar rcs libsmctrl.a libsmctrl.o
-  !nvcc -g -G -arch=sm_75 test_simple.cu -o test_simple libsmctrl.a -lcuda
+  gcc libsmctrl.c -c -o libsmctrl.o -fPIC
+  ar rcs libsmctrl.a libsmctrl.o
+  nvcc -g -G -arch=sm_75 test_simple.cu -o test_simple libsmctrl.a -lcuda
 */
 
 /*
@@ -24,6 +24,7 @@
     문제 1: argk의 type은 무엇?
         -> cuda runtime api가 '작동한다면', type의 크기와 offset은 알 수 있음
         그러면 uint_32t, uint_64t와 같은 '똑같은 크기의 type'으로 바꿀 수 있나?
+        -> 예제 상에서는 가능했음, 실제 orion-like 구현체 내에서는?
     문제 2: kernel_address의 type은 무엇?
         void (*func)(type1, type2, .. typeN)을 dynamic하게 구현할 수 있을까?
 
@@ -33,11 +34,13 @@
 #include <iostream>
 #include <stdio.h>
 #include "libsmctrl.h"
+#include <cuda_runtime.h>
+#include <cuda.h>
 
 // Let's say we don't have this functionality.
-typedef void (*func_ptr_t)(uint64_t, uint64_t, uint32_t);
+typedef void (*func_ptr_t)(uint32_t, uint64_t, uint64_t, uint32_t, uint32_t);
 
-__global__ void fun2(double* A, double B, float C) {
+__global__ void fun2(float Z, double* A, double B, float C, float D) {
     int workIndex = threadIdx.x + blockDim.x * blockIdx.x;
     A[workIndex] += 100.0;
     A[workIndex] += B;
@@ -48,10 +51,24 @@ __global__ void fun2(double* A, double B, float C) {
 
 __device__ func_ptr_t fun2_ptr;
 
-__global__ void wrapper(double* A, double B, float C) {
+__global__ void wrapper(float Z, double* A, double B, float C, float D) {
     int workIndex = threadIdx.x + blockDim.x * blockIdx.x;
     if (workIndex < 100 || workIndex > 800) return;
-    fun2_ptr((uint64_t)A, (uint64_t)B, (uint32_t)C);
+    fun2_ptr((uint32_t)Z, (uint64_t)A, (uint64_t)B, (uint32_t)C, (uint32_t)D);
+}
+
+/*
+    fun2_ptr에 대한 type을 지정하는 함수.
+
+*/
+void type_assign() {
+    size_t count = 0;
+    size_t offset;
+    size_t size;
+    while(cudaFuncGetParamInfo((const void*)fun2, count, &offset, &size) == cudaSuccess) {
+        printf("count: %ld, offset: %ld, size: %ld\n", count, offset, size);
+        count++;
+    }
 }
 
 
@@ -59,10 +76,12 @@ int main() {
     libsmctrl_test();
     test_run = 1;
 
+    type_assign();
+
     double* A = nullptr;
     cudaMallocManaged(&A, 1024 * sizeof(double));
 
-    fun2<<<4, 256>>>(A, 0.0, 0.0);
+    fun2<<<4, 256>>>(0.0, A, 0.0, 0.0, 0.0);
     cudaDeviceSynchronize();
 
     func_ptr_t h_fun2_dev_addr = (func_ptr_t)kernel_ptrs[0];
@@ -72,7 +91,7 @@ int main() {
 
     printf("2. fun2_ptr = %p\n", h_fun2_dev_addr);
 
-    wrapper<<<4, 256>>>(A, 50.0, 50.0);
+    wrapper<<<4, 256>>>(0.0, A, 50.0, 50.0, 0.0);
 
     cudaDeviceSynchronize();
     printf("are we alive?\n");
